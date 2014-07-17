718c450e0c13a57c773a9f25c35d399ad3e96dfc
ertheis <ertheis@Erics-MacBook-Pro.local>
2014-07-16 17:29:41 -0700

    Initial Commit

---
+++ DataSyncTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DataSyncTest
+//
+//  Created by Eric Theis on 7/16/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DataSyncTestTests/DataSyncTestTests.m
@@ -0,0 +1,34 @@
+//
+//  DataSyncTestTests.m
+//  DataSyncTestTests
+//
+//  Created by Eric Theis on 7/16/14.
+//  Copyright (c) 2014 PubNub. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataSyncTestTests : XCTestCase
+
+@end
+
+@implementation DataSyncTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
